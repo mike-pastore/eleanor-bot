e7b898a1dc6120a2529430851193e16b73a6b9d9
Mike Pastore <mike.t.pastore@gmail.com>
2016-10-30 21:37:06 -0500

    Github init

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var PORT = process.env.PORT || 3000;
+var TRAIN_API_KEY = "b5af096f27494b66acff8ce897c3d7c1";
+var BUS_API_KEY = "EDBN8TzSKyRbiJJb4T3s4u6XE";
 
 app.get('/', function(req, res) {
 	res.send('CTA Bot Webhook root');
